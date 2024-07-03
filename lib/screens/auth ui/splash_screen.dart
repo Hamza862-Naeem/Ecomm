4e4fbe00e6154f1174349ac0b06e55260273d07a
Hamza862-Naeem <mhamzanaeem862@gmail.com>
2024-07-03 17:59:51 +0500

    user logged in logic added

--- lib/screens/auth ui/splash_screen.dart
+++ lib/screens/auth ui/splash_screen.dart
@@ -3,9 +3,12 @@
 
 import 'dart:async';
 
+import 'package:ecomm/controllers/get-user-data-controller.dart';
+import 'package:ecomm/screens/admin%20panel/admin%20main%20screen.dart';
 import 'package:ecomm/screens/auth%20ui/user%20panel/main%20screen.dart';
 import 'package:ecomm/screens/auth%20ui/welcome_screen.dart';
 import 'package:ecomm/utils/app-constants.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -19,13 +22,30 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  User? user = FirebaseAuth.instance.currentUser;
   @override
-  void initState(){
+  void initState() {
     super.initState();
-    Timer(Duration(seconds: 3),(){
-      Get.offAll(() => WelcomeScreen());
+    Timer(Duration(seconds: 3), () {
+      loggdin(context);
     });
   }
+    Future<void> loggdin(BuildContext context)async{
+      if(user != null){
+        final GetUserDataController getUserDataController=
+        Get.put(GetUserDataController());
+        var userData = await getUserDataController.getUserData(user!.uid);
+        if(userData[0]['isAdmin'] == true){
+           Get.offAll(() => AdminMainScreen());
+        }else{
+          Get.offAll(() => MainScreen());
+        }
+
+      }else{
+        Get.to(()=> WelcomeScreen());
+      }
+    }
+
   Widget build(BuildContext context) {
     return  Scaffold(
       backgroundColor: AppConstant.appSecondaryColor,
