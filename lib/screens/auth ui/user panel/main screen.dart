6cec9c7b1c1a882021ac1d1447058c21b3bb1203
Hamza862-Naeem <mhamzanaeem862@gmail.com>
2024-07-03 22:59:25 +0500

    drawer widget

--- lib/screens/auth ui/user panel/main screen.dart
+++ lib/screens/auth ui/user panel/main screen.dart
@@ -1,6 +1,7 @@
 
 import 'package:ecomm/screens/auth%20ui/welcome_screen.dart';
 import 'package:ecomm/utils/app-constants.dart';
+import 'package:ecomm/widgets/custom-drawer.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -14,31 +15,18 @@ class MainScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        iconTheme: IconThemeData(
+          color: AppConstant.appTextColor,
+        ),
         systemOverlayStyle: SystemUiOverlayStyle(
           statusBarColor: AppConstant.appSecondaryColor,
           statusBarIconBrightness: Brightness.light),
-        title: Text(AppConstant.appMainName),
+        title: Text(AppConstant.appMainName,
+        style: TextStyle(color: AppConstant.appTextColor),),
         backgroundColor: AppConstant.appMainColor,
         centerTitle: true,
-        actions: [
-          GestureDetector(
-            onTap: () async{
-              GoogleSignIn googleSignIn = GoogleSignIn();
-              FirebaseAuth _auth= FirebaseAuth.instance;
-               await _auth.signOut();
-              await FirebaseAuth.instance.signOut();
-
-            // GoogleSignIn googleSignIn = GoogleSignIn();
-             // await googleSignIn.signIn();
-              Get.offAll(()=> WelcomeScreen());
-            },
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Icon(Icons.logout),
-            ),
-          )
-        ],
       ),
+      drawer: DrawerWidget(),
     );
   }
 }
